749d7e653127abf9a3cc90034c75ea30895690a2
Evgeny Shemchik <schemtschik@gmail.com>
2019-11-07 21:23:45 +0300

    fix

--- create.sql
+++ create.sql
@@ -1,55 +1,61 @@
-CREATE TABLE dates (
-    id INT NOT NULL PRIMARY KEY,
-    year INT NOT NULL,
-    month INT NOT NULL,
-    day INT NOT NULL
-);
-
-CREATE TABLE products (
-    id INT NOT NULL PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    issue_date INT NOT NULL REFERENCES dates(id)
-);
-
-CREATE TABLE game_families (
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    fans_count int DEFAULT 0,
-    games_count int DEFAULT 0,
-    first_issue_date INT NOT NULL REFERENCES dates(id)
-);
-
-CREATE TABLE game_designers (
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    country VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    found_date INT NOT NULL REFERENCES dates(id)
-);
-
 CREATE TABLE games (
     id INT NOT NULL PRIMARY KEY,
+    bgg_url VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    mechanics VARCHAR(255) NOT NULL,
-    players_count INT NOT NULL,
-    players_age INT NOT NULL,
-    price FLOAT NOT NULL,
-    rating FLOAT NOT NULL,
-    issue_date INT NOT NULL REFERENCES dates(id),
-    designer INT NOT NULL REFERENCES game_designers(id) ON DELETE CASCADE
+    min_players INT NOT NULL,
+    max_players INT NOT NULL,
+    avg_time INT NOT NULL,
+    min_time INT NOT NULL,
+    max_time INT NOT NULL,
+    year INT NOT NULL,
+    avg_rating FLOAT NOT NULL,
+    geek_rating FLOAT NOT NULL,
+    num_votes INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    owned INT NOT NULL
 );
 
-CREATE TABLE game_to_product (
+CREATE TABLE families (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE categories (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE designers (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE mechanics (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE game_to_designers (
     game_id INT NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-    PRIMARY KEY (game_id, product_id)
+    designer_id INT NOT NULL REFERENCES designers(id) ON DELETE CASCADE,
+    PRIMARY KEY (game_id, designer_id)
 );
 
 CREATE TABLE game_to_family (
     game_id INT NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    family_id INT NOT NULL REFERENCES game_families(id) ON DELETE CASCADE,
+    family_id INT NOT NULL REFERENCES families(id) ON DELETE CASCADE,
     PRIMARY KEY (game_id, family_id)
+);
+
+CREATE TABLE game_to_category (
+    game_id INT NOT NULL REFERENCES games(id) ON DELETE CASCADE,
+    category_id INT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+    PRIMARY KEY (game_id, category_id)
+);
+
+CREATE TABLE game_to_mechanics (
+    game_id INT NOT NULL REFERENCES games(id) ON DELETE CASCADE,
+    mechanic_id INT NOT NULL REFERENCES mechanics(id) ON DELETE CASCADE,
+    PRIMARY KEY (game_id, mechanic_id)
 );
