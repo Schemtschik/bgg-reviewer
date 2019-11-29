bb328fb85a9183161d802f00f557886af895ef16
Evgeny Shemchik <schemtschik@gmail.com>
2019-11-29 23:33:13 +0300

    front initial

---
+++ front/src/components/Navbars/AdminNavbarLinks.jsx
@@ -0,0 +1,41 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "react";
+import { NavItem, Nav } from "react-bootstrap";
+
+class AdminNavbarLinks extends Component {
+  render() {
+    return (
+      <div>
+        <Nav>
+          <NavItem eventKey={3} href="#">
+            <i className="fa fa-search" />
+            <p className="hidden-lg hidden-md">Search</p>
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={3} href="#">
+            Log out
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  }
+}
+
+export default AdminNavbarLinks;
